f0e0e1cb8b22ddc2ff4d744a189cb395051d8c7e
git <git@140.115.152.39>
2016-09-03 20:22:44 +0800

    test

--- js/my_first_javascript.js
+++ js/my_first_javascript.js
@@ -1,11 +1,21 @@
 alert("This is in javascript");
 
-var test = new function(){
+function test(){
 	var A = 1;
 
 	this.getA = function(){
 		return A;
 	}
 }
+
+var test2 = new function (){
+	var A = 1;
+
+	this.getA = function(){
+		return A;
+	}
+}
+
 console.log(test); //------------------------1
-console.log(test.getA()); //---------------------------2
+console.log(test2);
+console.log(test2.getA()); //---------------------------2
