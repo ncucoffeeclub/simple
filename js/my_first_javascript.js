2d62ae4919307cf72e8f5511435d7258b7a262c3
git <git@140.115.152.39>
2016-09-03 20:14:47 +0800

    test

--- js/my_first_javascript.js
+++ js/my_first_javascript.js
@@ -1,1 +1,11 @@
 alert("This is in javascript");
+
+function test(){
+	var a = 1;
+
+	function geta(){
+		return a;
+	}
+}
+console.log(test); //------------------------1
+console.log(test.geta()); //---------------------------2
